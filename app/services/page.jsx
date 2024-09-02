a79e7c7bb4ba826b898c7cc07bac7cfb90e7e59e
Caio_mizuno <caio.mizuno@me2.com.br>
2024-09-01 23:51:12 -0400

    fix - ajustando renderização da animação

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -7,13 +7,13 @@ import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <section className="min-h-[70vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            tansition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            transition: { delay: 2.4, durateion: 0.4, ease: "easeIn" },
           }}
           className="grid grid-cols-1 gap-[60px] md:grid-cols-2"
         >
