a02240dc8f24cd41bf5cda5b28ec2e1b92273d18
Caio_mizuno <caio.mizuno@me2.com.br>
2024-09-01 23:28:29 -0400

    update - ajustando services utils

--- components/utils/services.jsx
+++ components/utils/services.jsx
@@ -1,80 +1,40 @@
-// const services = [
-//   {
-//     num: "01",
-//     title: "Sicredi - Analista de desenvolvimento",
-//     description:
-//       "Contratado para atuar junto a equipe de desenvolvimento de sistemas e BI, implementando novas funcionalidades e automações para auxiliar colaboradores em suas tarefas diárias.",
-//     href: "/services/sicredi",
-//   },
-//   {
-//     num: "02",
-//     title: "Encode - Desenvolvedor Fullstack",
-//     description: "We provide the best web development services in the market.",
-//     href: "/services/web-development",
-//   },
-//   {
-//     num: "03",
-//     title: "Innyx - Desenvolvedor Freelancer",
-//     description: "We provide the best web development services in the market.",
-//     href: "/services/web-development",
-//   },
-//   {
-//     num: "04",
-//     title: "ME2 - Desenvolvedor Fullstack|Squad Leader",
-//     description: "We provide the best web development services in the market.",
-//     href: "/services/web-development",
-//   },
-//   {
-//     num: "05",
-//     title: "Samulife - Desenvolvedor Freelancer",
-//     description: "We provide the best web development services in the market.",
-//     href: "/services/web-development",
-//   },
-//   {
-//     num: "06",
-//     title: "Hermes - Desenvolvedor Freelancer",
-//     description: "We provide the best web development services in the market.",
-//     href: "/services/web-development",
-//   },
-// ];
-
 const services = [
   {
     num: "01",
     title: "Back-end",
     description:
       "Atuando no desenvolvimento de APIs e integrações de sistemas.",
-    href: "/services/sicredi",
+    href: "/services/backend",
   },
   {
     num: "02",
     title: "Front-end",
     description: "Atuando no desenvolvimento de interfaces de usuário para diversos tipos de aplicações, como sistema de e-commerce.",
-    href: "/services/web-development",
+    href: "/services/frontend",
   },
   {
     num: "03",
     title: "Arquiteto de Software",
     description: "Atuando na definição da estrutura de um sistema, como ele será dividido em módulos e como esses módulos se comunicarão entre si.",
-    href: "/services/web-development",
+    href: "/services/architecture",
   },
   {
     num: "04",
     title: "UI/UX Design",
     description: "Desenvolvendo mockups e wireframes para aplicações web e mobile.",
-    href: "/services/web-development",
+    href: "/services/uiux",
   },
   {
     num: "05",
     title: "Automações",
     description: "Desenvolvendo automações para facilitar nas ações manuais e nas rotinas dos stackholders.",
-    href: "/services/web-development",
+    href: "/services/automation",
   },
   {
       num: "06",
       title: "Business Intelligence",
       description: "Atuando na produção de material analítico para tomada de decisões.",
-      href: "/services/web-development",
+      href: "/services/bi",
     },
 ];
 
